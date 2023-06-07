78bcd0a76e2581035ea2d2364cd324d36e0b2482
KyleCe <kylece@psyai.net>
2023-06-07 16:57:13 +0800

    [add] first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# WebSDKdemo 
+
+2 D human WebSDK demo 
+
+## config
+config the ak, sk and userId in index.html
+```javascript
+	const userId = '0086*******1904'
+	const ak = '*******'
+	const sk = '*******'
+```
+
+## build
+
+```shell
+npm i
+```
+
+## run
+
+```shell
+nodemon serer.js
+```
+
+## test
+
+open http://localhost:8081 in browser
---
+++ server.js
@@ -0,0 +1,14 @@
+﻿const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8081;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, '0.0.0.0', () => {
+	console.log(`Server running at http://localhost:${port}/`);
+}); 
